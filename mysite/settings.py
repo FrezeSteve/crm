aca1c22637c7578ea6f41d1666f868c8a8636f1c
“frezesteve” <stevefrisson@gmail.com>
2020-01-18 12:18:42 +0300

    static settings back to default

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "lead/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    "/home/steve/PycharmProjects/Leads/mysite/static",
-]
+# STATICFILES_DIRS = [
+#     "/home/steve/PycharmProjects/Leads/mysite/static",
+# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
